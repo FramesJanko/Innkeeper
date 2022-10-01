cfaa6ddad67bd5878d831a799b22822d71eb5c29
FramesJanko <70084922+FramesJanko@users.noreply.github.com>
2022-09-30 22:02:48 -0400

    working on stat models

---
+++ InnkeeperAPIApp/InnkeeperWPFUserInterface.Library/Models/CharacterClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnkeeperWPFUserInterface.Library.Models
+{
+    public class CharacterClass
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string savingthrow1;
+
+        public string SavingThrow1
+        {
+            get { return savingthrow1; }
+            set { savingthrow1 = value; }
+        }
+        private string savingthrow2;
+
+        public string SavingThrow2
+        {
+            get { return savingthrow2; }
+            set { savingthrow2 = value; }
+        }
+
+        public CharacterClass(string className)
+        {
+            Name = className;
+            switch (className)
+            {
+                case "Artificer":
+                    SavingThrow1 = "ConstitutionSave";
+                    SavingThrow2 = "IntelligenceSave";
+                    break;
+                case "Bard":
+                    SavingThrow1 = "DexteritySave";
+                    SavingThrow2 = "CharismaSave";
+                    break;
+                case "Barbarian":
+                    SavingThrow1 = "StrengthSave";
+                    SavingThrow2 = "ConstitutionSave";
+                    break;
+                case "Cleric":
+                    SavingThrow1 = "WisdomSave";
+                    SavingThrow2 = "CharismaSave";
+                    break;
+                case "Druid":
+                    SavingThrow1 = "WisdomSave";
+                    SavingThrow2 = "IntelligenceSave";
+                    break;
+                case "Fighter":
+                    SavingThrow1 = "StrengthSave";
+                    SavingThrow2 = "ConstitutionSave";
+                    break;
+                case "Monk":
+                    SavingThrow1 = "Strengthave";
+                    SavingThrow2 = "DexteritySave";
+                    break;
+                case "Paladin":
+                    SavingThrow1 = "WisdomSave";
+                    SavingThrow2 = "CharismaSave";
+                    break;
+                case "Ranger":
+                    SavingThrow1 = "StrengthSave";
+                    SavingThrow2 = "DexteritySave";
+                    break;
+                case "Rogue":
+                    SavingThrow1 = "DexteritySave";
+                    SavingThrow2 = "IntelligenceSave";
+                    break;
+                case "Sorcerer":
+                    SavingThrow1 = "ConstitutionSave";
+                    SavingThrow2 = "CharismaSave";
+                    break;
+                case "Warlock":
+                    SavingThrow1 = "WisdomSave";
+                    SavingThrow2 = "CharismaSave";
+                    break;
+                case "Wizard":
+                    SavingThrow1 = "WisdomSave";
+                    SavingThrow2 = "IntelligenceSave";
+                    break;
+            }
+        
+        }
+    }
+}
---
+++ InnkeeperAPIApp/InnkeeperWPFUserInterface.Library/Models/SavingThrowModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnkeeperWPFUserInterface.Library.Models
+{
+    public class SavingThrowModel
+    {
+        public int StrengthSave { get; set; }
+        public int DexteritySave { get; set; }
+        public int ConstitutionSave { get; set; }
+        public int IntelligenceSave { get; set; }
+        public int WisdomSave { get; set; }
+        public int CharismaSave { get; set; }
+    }
+}
